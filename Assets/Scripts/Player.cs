f05be3aaec2bd0bf3cd8636c286bc794cc70cb18
razhollander <50023823+razhollander@users.noreply.github.com>
2019-09-22 11:45:54 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField]
+    GameObject car;
+    [SerializeField]
+    float rotationSpeed = 1;
+    [SerializeField]
+    float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        car.transform.Rotate(0, Input.GetAxis("Horizontal")* rotationSpeed*Time.deltaTime, 0);
+        rb.AddForce(-car.transform.forward* speed* Input.GetAxis("Vertical"));
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Booster
+ unity project
