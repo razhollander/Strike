de71912e9bbec0426bf0c1d470c856dcdae473b6
razhollander <50023823+razhollander@users.noreply.github.com>
2020-06-14 19:50:12 +0300

    fixed the radius bug

--- Assets/Scripts/Player/Appearance/Radius/RadiusUpgradesAppearance.cs
+++ Assets/Scripts/Player/Appearance/Radius/RadiusUpgradesAppearance.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RadiusUpgradesAppearance : MonoBehaviour
 {
+    private float startScale = 1;
+    private void Awake()
+    {
+        startScale = transform.localScale.x;
+    }
     void Start()
     {
         GameManager.Instance.GameStateManager.GetState<NormalPlayState>().OnEnter += InitAppearance;
@@ -12,7 +17,7 @@ public class RadiusUpgradesAppearance : MonoBehaviour
     void InitAppearance()
     {
         float radiusUpgradeLevel = GameManager.Instance.UpgradesManager.GetUpgrade<RadiusUpgrader>().GetUpgradeValue();
-        Vector3 newScale = Vector3.one * radiusUpgradeLevel;
+        Vector3 newScale = Vector3.one * radiusUpgradeLevel * startScale;
         transform.localScale = newScale;
     }
 
